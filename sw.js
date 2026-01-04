70e265ccfc92ec8ef710412b5eb14f28de9dd192
Chronicle User <user@chronicle.local>
2026-01-04 00:16:22 -0500

    Landscape: move current card up (top:15px) - v1.3.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Chronicle PWA Service Worker v1.0.4
-const CACHE_NAME = 'chronicle-v23';
+const CACHE_NAME = 'chronicle-v24';
 const ASSETS_TO_CACHE = [
   './manifest.json',
   './icon-192.png',
