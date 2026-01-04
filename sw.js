bc6a6eb6cc3bef43fdf1c6cfd0cf1450e3ed8845
Chronicle User <user@chronicle.local>
2026-01-04 00:58:17 -0500

    v2.1 - Revert timeline, center current card horizontally

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Chronicle PWA Service Worker v2.0
-const CACHE_NAME = 'chronicle-v2.0';
+// Chronicle PWA Service Worker v2.1
+const CACHE_NAME = 'chronicle-v2.1';
 const ASSETS_TO_CACHE = [
   './manifest.json',
   './icon-192.png',
