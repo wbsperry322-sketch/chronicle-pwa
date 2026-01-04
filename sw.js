b2aa761a3a3bfba82a1ba176f3aa2af4f8e03164
Chronicle User <user@chronicle.local>
2026-01-04 00:49:42 -0500

    Revert to working v77 code with top-right landscape current card

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Chronicle PWA Service Worker v1.1.0
-const CACHE_NAME = 'chronicle-v33';
+// Chronicle PWA Service Worker v1.1.1
+const CACHE_NAME = 'chronicle-v34';
 const ASSETS_TO_CACHE = [
   './manifest.json',
   './icon-192.png',
