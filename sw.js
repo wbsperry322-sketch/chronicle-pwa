276ca3c5c4d0b911baccd442981ad58f408c2a16
Chronicle User <user@chronicle.local>
2026-01-04 02:32:08 -0500

    v3.5.3: Fix drag CSS - use position:fixed !important and higher z-index

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Chronicle PWA Service Worker v3.5.2
-const CACHE_NAME = 'chronicle-v3.5.2';
+// Chronicle PWA Service Worker v3.5.3
+const CACHE_NAME = 'chronicle-v3.5.3';
 const ASSETS_TO_CACHE = [
   './manifest.json',
   './icon-192.png',
