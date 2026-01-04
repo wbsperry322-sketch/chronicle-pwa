5590b9ed3008b75b6c5bf26e9e37b35b4a0d86b1
Chronicle User <user@chronicle.local>
2026-01-04 00:24:23 -0500

    Re-add current event box outside wrapper, fixed bottom center default, top-right landscape - v1.3.5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Chronicle PWA Service Worker v1.0.4
-const CACHE_NAME = 'chronicle-v26';
+const CACHE_NAME = 'chronicle-v27';
 const ASSETS_TO_CACHE = [
   './manifest.json',
   './icon-192.png',
