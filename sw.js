30a4d72b693e6155598cab69f468dd13a53dac70
Chronicle User <user@chronicle.local>
2026-01-03 23:33:28 -0500

    Fix vertical centering for all modes, timeline bar 50px thick - v1.2.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Chronicle PWA Service Worker v1.0.4
-const CACHE_NAME = 'chronicle-v11';
+const CACHE_NAME = 'chronicle-v12';
 const ASSETS_TO_CACHE = [
   './manifest.json',
   './icon-192.png',
