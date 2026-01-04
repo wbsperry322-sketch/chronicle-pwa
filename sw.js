fa14d4aedfd37a14a7b13d69f01c8df643411dce
Chronicle User <user@chronicle.local>
2026-01-04 00:09:44 -0500

    Use #game-screen .game-center selector, update landscape to match - v1.3.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Chronicle PWA Service Worker v1.0.4
-const CACHE_NAME = 'chronicle-v21';
+const CACHE_NAME = 'chronicle-v22';
 const ASSETS_TO_CACHE = [
   './manifest.json',
   './icon-192.png',
