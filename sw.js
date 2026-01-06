f69200a0a6b4ddafc19f1dd75e582c15760233c4
Chronicle User <user@chronicle.local>
2026-01-06 18:54:06 -0500

    v7.1: Desktop leave button aligned with banner, hide mistakes tray on home screen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Chronicle PWA Service Worker v7.0
-const CACHE_NAME = 'chronicle-v7.0';
+// Chronicle PWA Service Worker v7.1
+const CACHE_NAME = 'chronicle-v7.1';
 const ASSETS_TO_CACHE = [
   './manifest.json',
   './icon-192.png',
