5d3ef1e063323afe13a6d8798d7aea20a9eee4c0
Chronicle User <user@chronicle.local>
2026-01-06 19:38:29 -0500

    v6.6: Move leave button up to banner level, style leave lobby button as primary

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Chronicle PWA Service Worker v6.5
-const CACHE_NAME = 'chronicle-v6.5';
+const CACHE_NAME = 'chronicle-v6.6';
 const ASSETS_TO_CACHE = [
   './manifest.json',
   './icon-192.png',
